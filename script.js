0a852e9ae9e6ae722b7e30b4e6efcd7f54f49515
Zach Neibaur <znn117@hotmail.com>
2019-03-29 07:40:07 -0600

    Fixing initial screens size

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-function beginTree() {
+function setCanvasSize() {
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   document.getElementById('treeCanvas').width = windowWidth - 100; // Set the Canvas size to better fit the window.
   document.getElementById('treeCanvas').height = windowHeight - 200;
+}
+
+function beginTree() {
   const horizontal = document.getElementById('treeCanvas').width; // Set horizontal and vertical values to be used else where in the app.
   const vertical = document.getElementById('treeCanvas').height / 2;
   const username = document.getElementById('username').value;
   const user = getGithubData(username); // Pass the username and any parameters to the function that retrieves github info.
-  const followers = getGithubData(username, '/followers');
-  const following = getGithubData(username, '/following');
   const repos = getGithubData(username, '/repos');
   drawUserNode(horizontal, vertical);
 }
 
 function getGithubData(username, request = '') {
   let data;
-  // fetch(`https://api.github.com/users/${username}${request}`)
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(responseJSON) {
-  //     for (const item of Object.keys(responseJSON)) {
-  //       data = responseJSON[item];
-  //     }
-  //   })
-  // console.log(data);
+  fetch(`https://api.github.com/users/${username}${request}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(responseJSON) {
+      console.log(responseJSON);
+      // for (const item of Object.keys(responseJSON)) {
+      //   data = responseJSON[item];
+      //   console.log(data);
+      // }
+    })
   return data;
 }
 
 function drawUserNode(horizontal, vertical) {
   const div = document.createElement('div');
-  div.className = 'userNode';
+  div.className = 'node';
   const canvas = document.getElementById('treeCanvas');
   canvas.insertAdjacentElement('afterend', div);
-  const userNode = document.querySelector('.userNode');
+  const userNode = document.querySelector('.node');
   horizontal = horizontal * .05;
   userNode.style.left = `${horizontal}px`;
   userNode.style.top = `${vertical}px`;
@@ -46,4 +48,10 @@ function drawUserNode(horizontal, vertical) {
     drawLine.strokeStyle = '#565656';
     drawLine.stroke();
   }
+}
+
+function drawRepoNode(horizontal, vertical) {
+  const div = document.createElement('div');
+  div.className = 'node'
+  const canvas = document.getElementById('treeCanvas');
 }
